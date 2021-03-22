7999f586d90a73431934415888c434ee60525ba6
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2021-03-22 18:59:01 -0300

    Initial commit

---
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -0,0 +1,60 @@
+'use strict'
+
+const crypto = require('crypto')
+const moment = require('moment')
+
+const User = use('App/Models/User')
+const Mail = use('Mail')
+
+class ForgotPasswordController {
+  async store ({ request, response }) {
+    try {
+      const email = request.input('email')
+      const user = await User.findByOrFail('email', email)
+
+      user.token = crypto.randomBytes(10).toString('hex')
+      user.token_created_at = new Date()
+
+      await user.save()
+
+      await Mail.send(
+        ['emails.forgot_password'],
+        { email, token: user.token, link: `${request.input('redirect_url')}?token=${user.token}` },
+        message => {
+          message
+            .to(user.email)
+            .from('user@example.com', 'Framework AdonisJS')
+            .subject('Password retrieving')
+        }
+      )
+    } catch (err) {
+      return response.status(err.status).send({ error: { message: 'Something went wrong. Is this email a valid one?' } })
+    }
+  }
+
+  async update ({ request, response }) {
+    try {
+      const { token, password } = request.all()
+
+      const user = await User.findByOrFail('token', token)
+
+      const tokenExpired = moment()
+        .subtract('2', 'days')
+        .isAfter(user.token_created_at)
+
+      if (tokenExpired) {
+        return response.status(401).send({ error: { message: 'Token provided is expired!' } })
+      }
+
+      user.token = null
+      user.token_created_at = null
+      user.password = password
+
+      await user.save()
+    } catch (err) {
+      return response.status(err.status).send({ error: { message: 'Something went wrong trying to reset your password!' } })
+    }
+  }
+}
+
+module.exports = ForgotPasswordController
